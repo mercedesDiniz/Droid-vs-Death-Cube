d8ffb4a0ec2c14ac6c0210ae009a27b9f7d86e70
mercedesDiniz <mercy.diniz82@gmail.com>
2023-05-20 22:44:25 -0300

    Distribui ção das Food Pill e Power Pill + Rotinas

---
+++ Pacman/Assets/Scripts/rotinaPowerPill.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotinaPowerPill : MonoBehaviour
+{
+    public float scaleSpeed = 0.5f; // Velocidade de redução da escala
+    public float destroyDelay = 2f; // Tempo de espera antes de destruir o objeto após desaparecer
+
+    private Vector3 initialScale;
+    private bool isScaling = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Detecta colisão com os Inimigos
+    private void OnCollisionStay(Collision collision) {
+        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("InimigoR")){
+
+            if (isScaling){
+                // Reduz gradualmente a escala do objeto
+                transform.localScale -= new Vector3(scaleSpeed, scaleSpeed, scaleSpeed) * Time.deltaTime;
+
+                // Verifica se o objeto ficou invisível
+                if (transform.localScale.x <= 0f)
+                {
+                    isScaling = false;
+                    Destroy(gameObject);
+                }
+            }
+
+        }
+    }
+}
